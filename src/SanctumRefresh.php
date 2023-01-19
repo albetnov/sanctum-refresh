5cd966b08fc9aafa298bb8ba0d32c6c95b2b81eb
albetnov <ryxinfrvr@gmail.com>
2023-01-19 13:43:53 +0700

    feat: make sanctum refresh model as default

--- src/SanctumRefresh.php
+++ src/SanctumRefresh.php
@@ -3,7 +3,9 @@
 namespace Albet\SanctumRefresh;
 
 use Albet\SanctumRefresh\Controllers\AuthController;
+use Albet\SanctumRefresh\Models\PersonalAccessToken;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Sanctum;
 
 class SanctumRefresh
 {
@@ -13,5 +15,10 @@ class SanctumRefresh
             Route::post('/login', 'login');
             Route::post('/refresh', 'refresh')->middleware('checkRefreshToken');
         });
+    }
+
+    public static function configure()
+    {
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }
