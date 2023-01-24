b9fd84602213c49dedc4ee123584ade32b6a60e4
albetnov <ryxinfrvr@gmail.com>
2023-01-24 08:54:22 +0700

    fix: add missing import on tests

--- tests/FacadeSanctumRefreshTest.php
+++ tests/FacadeSanctumRefreshTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Albet\SanctumRefresh\Facades\SanctumRefresh as SanctumRefreshFacade;
+use Albet\SanctumRefresh\SanctumRefresh;
 
 it('Ensure facade has a correct accessor', function () {
     SanctumRefreshFacade::shouldReceive('routes')
