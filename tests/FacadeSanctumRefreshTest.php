9cfd1e641fabc2bc85635f14bfc47807829c0396
albetnov <ryxinfrvr@gmail.com>
2023-01-24 09:04:42 +0700

    fix: failed facade test

--- tests/FacadeSanctumRefreshTest.php
+++ tests/FacadeSanctumRefreshTest.php
@@ -1,11 +1,10 @@
 <?php
 
 use Albet\SanctumRefresh\Facades\SanctumRefresh as SanctumRefreshFacade;
-use Albet\SanctumRefresh\SanctumRefresh;
 
 it('Ensure facade has a correct accessor', function () {
     SanctumRefreshFacade::shouldReceive('routes')
     ->once();
 
-    SanctumRefresh::routes();
+    \SanctumRefresh::routes();
 });
